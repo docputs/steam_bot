072df6c77e498b8c77ed344824dd394327ebb3fe
João Vitor <docputs@gmail.com>
2020-05-22 03:29:39 -0200

    added database, functionalities and telegram commands

--- steamweb.py
+++ steamweb.py
@@ -1,15 +1,38 @@
 from steam.webapi import WebAPI
+from csv import DictReader
 
 
-def meusjogos(user_id=None):
+def find_steamid(userid):
+    with open('database.csv', 'r') as db:
+        dados = DictReader(f=db)
+        for linha in dados:
+            if linha['userid'] == str(userid):
+                steamid = linha['steamid']
+                return steamid
+
+
+def meulevel(steamid=None):
+    api = WebAPI(key='C20F32188B2E214DB83F1A3626E34889')
+
+    level = api.IPlayerService.GetSteamLevel(steamid=steamid)
+    level = level['response']['player_level']
+
+    return level
+
+
+def meusjogos(steamid=None):
     api = WebAPI(key='C20F32188B2E214DB83F1A3626E34889')
 
     jogos = 'Sua lista de jogos:\n\n'
 
-    level = api.IPlayerService.GetSteamLevel(steamid=user_id)
-    recentes = api.IPlayerService.GetRecentlyPlayedGames(steamid=user_id, count=0)
-    amigos = api.ISteamUser.GetFriendList(steamid=user_id)
-    return amigos
+    lista = api.IPlayerService.GetOwnedGames(steamid=steamid, include_appinfo=True,
+                                             include_played_free_games=False, appids_filter=False,
+                                             include_free_sub=False)
+
+    for jogo in lista['response']['games']:
+        jogos += f'<strong>{jogo["name"]}</strong>\n     {round(jogo["playtime_forever"]/60)} horas jogadas\n\n'
+
+    return jogos
 
 
 if __name__ == '__main__':
--- telegram.py
+++ telegram.py
@@ -1,8 +1,21 @@
 import telebot
 import steamweb
 import lista_promocoes
+from csv import DictWriter, DictReader
 
 bot = telebot.TeleBot('1148615135:AAE3Gtp3EOSgc0uMp2KRpomdyFgNS5J4hRQ')
+
+resposta_forcada = telebot.types.ForceReply()
+
+
+def steamid_message_check(msg):
+    msg = msg.text
+    msg = msg.split(' ')
+
+    if msg[0] == 'steamid' and msg[1].isnumeric():
+        return True
+    else:
+        return False
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -13,7 +26,12 @@ def send_welcome(msg):
 
 @bot.message_handler(commands=['meusjogos'])
 def send_jogos(msg):
-    bot.send_message(chat_id=msg.chat.id, text=steamweb.meusjogos(76561198067139679))
+    steamid = steamweb.find_steamid(msg.from_user.id)
+
+    if steamid is not None:
+        bot.send_message(chat_id=msg.chat.id, text=steamweb.meusjogos(steamid), parse_mode='HTML')
+    else:
+        bot.send_message(chat_id=msg.chat.id, text='Steam ID não encontrado :c')
 
 
 @bot.message_handler(commands=['promocoes'])
@@ -22,4 +40,22 @@ def show_specials(msg):
     print(f'{msg.from_user.first_name} solicitou as promocoes')
 
 
-bot.polling()
+@bot.message_handler(commands=['steamid'])
+def ask_steamid(msg):
+    bot.send_message(chat_id=msg.chat.id, text='Envie-me o seu Steam ID digitando steamid <numero>\n\n'
+                                               'Utilize shorturl.at/equxB para encontrar um Steam ID',
+                     reply_markup=resposta_forcada)
+
+
+@bot.message_handler(content_types=['text'], func=steamid_message_check)
+def save_steamid(msg):
+    with open('database.csv', 'a') as db:
+        escritor = DictWriter(f=db, fieldnames=['userid', 'steamid'])
+
+        lista = {'userid': msg.from_user.id, 'steamid': msg.text.split(' ')[1]}
+        escritor.writerow(lista)
+
+    bot.send_message(chat_id=msg.chat.id, text=f'Steam ID {msg.text.split(" ")[1]} cadastrado')
+
+
+bot.polling(timeout=20, none_stop=True)
